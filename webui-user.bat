8b3e71373e1880e46e2d38cadb86f05688837bf8
Haoming <hmstudy02@gmail.com>
2024-10-14 12:47:19 +0800

    bat

--- webui-user.bat
+++ webui-user.bat
@@ -3,16 +3,10 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS= --no-download-sd-model
 
-@REM Uncomment following code to reference an existing A1111 checkout.
-@REM set A1111_HOME=Your A1111 checkout dir
-@REM
-@REM set VENV_DIR=%A1111_HOME%/venv
-@REM set COMMANDLINE_ARGS=%COMMANDLINE_ARGS% ^
-@REM  --ckpt-dir %A1111_HOME%/models/Stable-diffusion ^
-@REM  --hypernetwork-dir %A1111_HOME%/models/hypernetworks ^
-@REM  --embeddings-dir %A1111_HOME%/embeddings ^
-@REM  --lora-dir %A1111_HOME%/models/Lora
+:: --xformers
+:: --pin-shared-memory --cuda-malloc --cuda-stream
+:: --skip-python-version-check --skip-torch-cuda-test --skip-version-check --skip-prepare-environment --skip-install
 
 call webui.bat
