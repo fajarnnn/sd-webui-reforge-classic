a97d8e345bd20c96d71c26493c4a4b648e88950c
Haoming <hmstudy02@gmail.com>
2024-12-12 11:56:47 +0800

    built-in

--- extensions-builtin/mobile/javascript/mobile.js
+++ extensions-builtin/mobile/javascript/mobile.js
@@ -1,34 +1,39 @@
-var isSetupForMobile = false;
+(function () {
 
-function isMobile() {
-    for (var tab of ["txt2img", "img2img"]) {
-        var imageTab = gradioApp().getElementById(tab + '_results');
-        if (imageTab && imageTab.offsetParent && imageTab.offsetLeft == 0) {
-            return true;
+    let isSetupForMobile = false;
+
+    function isMobile() {
+        for (let tab of ["txt2img", "img2img"]) {
+            let imageTab = gradioApp().getElementById(tab + '_results');
+            if (imageTab && imageTab.offsetParent && imageTab.offsetLeft == 0) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    function reportWindowSize() {
+        // not applicable for compact prompt layout
+        if (gradioApp().querySelector('.toprow-compact-tools')) return;
+
+        let currentlyMobile = isMobile();
+        if (currentlyMobile == isSetupForMobile) return;
+        isSetupForMobile = currentlyMobile;
+
+        for (let tab of ["txt2img", "img2img"]) {
+            let button = gradioApp().getElementById(tab + '_generate_box');
+            let target = gradioApp().getElementById(currentlyMobile ? tab + '_results' : tab + '_actions_column');
+            target.insertBefore(button, target.firstElementChild);
+
+            gradioApp().getElementById(tab + '_results').classList.toggle('mobile', currentlyMobile);
         }
     }
 
-    return false;
-}
+    window.addEventListener("resize", reportWindowSize);
 
-function reportWindowSize() {
-    if (gradioApp().querySelector('.toprow-compact-tools')) return; // not applicable for compact prompt layout
+    onUiLoaded(function () {
+        reportWindowSize();
+    });
 
-    var currentlyMobile = isMobile();
-    if (currentlyMobile == isSetupForMobile) return;
-    isSetupForMobile = currentlyMobile;
-
-    for (var tab of ["txt2img", "img2img"]) {
-        var button = gradioApp().getElementById(tab + '_generate_box');
-        var target = gradioApp().getElementById(currentlyMobile ? tab + '_results' : tab + '_actions_column');
-        target.insertBefore(button, target.firstElementChild);
-
-        gradioApp().getElementById(tab + '_results').classList.toggle('mobile', currentlyMobile);
-    }
-}
-
-window.addEventListener("resize", reportWindowSize);
-
-onUiLoaded(function() {
-    reportWindowSize();
-});
+})();
--- extensions-builtin/sd_forge_neveroom/scripts/forge_never_oom.py
+++ extensions-builtin/sd_forge_neveroom/scripts/forge_never_oom.py
@@ -1,7 +1,7 @@
-import gradio as gr
-
-from modules import scripts
 from ldm_patched.modules import model_management
+from modules import scripts
+
+import gradio as gr
 
 
 class NeverOOMForForge(scripts.Script):
@@ -19,18 +19,24 @@ class NeverOOMForForge(scripts.Script):
 
     def ui(self, *args, **kwargs):
         with gr.Accordion(open=False, label=self.title()):
-            unet_enabled = gr.Checkbox(label='Enabled for UNet (always maximize offload)', value=False)
-            vae_enabled = gr.Checkbox(label='Enabled for VAE (always tiled)', value=False)
+            unet_enabled = gr.Checkbox(
+                label="Enabled for UNet (always offload)",
+                value=False,
+            )
+            vae_enabled = gr.Checkbox(
+                label="Enabled for VAE (always tile)",
+                value=False,
+            )
         return unet_enabled, vae_enabled
 
     def process(self, p, *script_args, **kwargs):
         unet_enabled, vae_enabled = script_args
 
         if unet_enabled:
-            print('NeverOOM Enabled for UNet (always maximize offload)')
+            print("NeverOOM Enabled for UNet")
 
         if vae_enabled:
-            print('NeverOOM Enabled for VAE (always tiled)')
+            print("NeverOOM Enabled for VAE")
 
         model_management.VAE_ALWAYS_TILED = vae_enabled
 
@@ -41,7 +47,5 @@ class NeverOOMForForge(scripts.Script):
                 model_management.vram_state = model_management.VRAMState.NO_VRAM
             else:
                 model_management.vram_state = self.original_vram_state
-            print(f'VARM State Changed To {model_management.vram_state.name}')
+            print(f"VRAM State Changed To {model_management.vram_state.name}")
             self.previous_unet_enabled = unet_enabled
-
-        return
