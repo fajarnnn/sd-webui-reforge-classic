e31eb515be05956259e6ac7fc861509e93fa8ee2
Haoming <hmstudy02@gmail.com>
2024-12-13 15:21:52 +0800

    low

--- modules/sd_models_types.py
+++ modules/sd_models_types.py
@@ -9,16 +9,13 @@ if TYPE_CHECKING:
 class WebuiSdModel(LatentDiffusion):
     """This class is not actually instantinated, but its fields are created and fieeld by webui"""
 
-    lowvram: bool
-    """True if lowvram/medvram optimizations are enabled -- see modules.lowvram for more info"""
-
     sd_model_hash: str
     """short hash, 10 first characters of SHA1 hash of the model file; may be None if --no-hashing flag is used"""
 
     sd_model_checkpoint: str
     """path to the file on disk that model weights were obtained from"""
 
-    sd_checkpoint_info: 'CheckpointInfo'
+    sd_checkpoint_info: "CheckpointInfo"
     """structure with additional information about the file with model's weights"""
 
     is_sdxl: bool
