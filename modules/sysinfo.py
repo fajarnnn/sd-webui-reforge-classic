a4d74847a4e4e2483fee8709fc8d3f6f9c302f33
Haoming <hmstudy02@gmail.com>
2024-12-18 16:25:28 +0800

    sys

--- modules/sysinfo.py
+++ modules/sysinfo.py
@@ -1,18 +1,18 @@
-import json
-import os
-import sys
-
-import platform
-import hashlib
-import pkg_resources
-import psutil
-import re
-
-import launch
 from modules import paths_internal, timer, shared, extensions, errors
 
-checksum_token = "DontStealMyGamePlz__WINNERS_DONT_USE_DRUGS__DONT_COPY_THAT_FLOPPY"
-environment_whitelist = {
+import pkg_resources
+import platform
+import hashlib
+import psutil
+import json
+import sys
+import re
+import os
+
+
+TOKEN = "EPIC_BRUH_MOMENT"
+
+ENV_WHITELIST = {
     "GIT",
     "INDEX_URL",
     "WEBUI_LAUNCH_LIVE_OUTPUT",
@@ -37,20 +37,17 @@ environment_whitelist = {
 
 def pretty_bytes(num, suffix="B"):
     for unit in ["", "K", "M", "G", "T", "P", "E", "Z", "Y"]:
-        if abs(num) < 1024 or unit == 'Y':
+        if abs(num) < 1024 or unit == "Y":
             return f"{num:.0f}{unit}{suffix}"
         num /= 1024
 
 
 def get():
     res = get_dict()
-
     text = json.dumps(res, ensure_ascii=False, indent=4)
 
     h = hashlib.sha256(text.encode("utf8"))
-    text = text.replace(checksum_token, h.hexdigest())
-
-    return text
+    return text.replace(TOKEN, h.hexdigest())
 
 
 re_checksum = re.compile(r'"Checksum": "([0-9a-fA-F]{64})"')
@@ -61,7 +58,7 @@ def check(x):
     if not m:
         return False
 
-    replaced = re.sub(re_checksum, f'"Checksum": "{checksum_token}"', x)
+    replaced = re.sub(re_checksum, f'"Checksum": "{TOKEN}"', x)
 
     h = hashlib.sha256(replaced.encode("utf8"))
     return h.hexdigest() == m.group(1)
@@ -73,12 +70,11 @@ def get_dict():
     res = {
         "Platform": platform.platform(),
         "Python": platform.python_version(),
-        "Version": launch.git_tag(),
-        "Commit": launch.commit_hash(),
+        "Version": "classic",
         "Script path": paths_internal.script_path,
         "Data path": paths_internal.data_path,
         "Extensions dir": paths_internal.extensions_dir,
-        "Checksum": checksum_token,
+        "Checksum": TOKEN,
         "Commandline": get_argv(),
         "Torch env info": get_torch_sysinfo(),
         "Exceptions": errors.get_exceptions(),
@@ -88,21 +84,34 @@ def get_dict():
             "count physical": psutil.cpu_count(logical=False),
         },
         "RAM": {
-            x: pretty_bytes(getattr(ram, x, 0)) for x in ["total", "used", "free", "active", "inactive", "buffers", "cached", "shared"] if getattr(ram, x, 0) != 0
+            x: pretty_bytes(getattr(ram, x, 0))
+            for x in [
+                "total",
+                "used",
+                "free",
+                "active",
+                "inactive",
+                "buffers",
+                "cached",
+                "shared",
+            ]
+            if getattr(ram, x, 0) != 0
         },
         "Extensions": get_extensions(enabled=True),
         "Inactive extensions": get_extensions(enabled=False),
         "Environment": get_environment(),
         "Config": get_config(),
         "Startup": timer.startup_record,
-        "Packages": sorted([f"{pkg.key}=={pkg.version}" for pkg in pkg_resources.working_set]),
+        "Packages": sorted(
+            [f"{pkg.key}=={pkg.version}" for pkg in pkg_resources.working_set]
+        ),
     }
 
     return res
 
 
 def get_environment():
-    return {k: os.environ[k] for k in sorted(os.environ) if k in environment_whitelist}
+    return {k: os.environ[k] for k in sorted(os.environ) if k in ENV_WHITELIST}
 
 
 def get_argv():
@@ -121,15 +130,35 @@ def get_argv():
 
     return res
 
+
 re_newline = re.compile(r"\r*\n")
+
+
+def _val(v: str):
+    v = str(v).strip()
+    if not v:
+        return None
+    try:
+        return int(v)
+    except ValueError:
+        return v
 
 
 def get_torch_sysinfo():
     try:
         import torch.utils.collect_env
-        info = torch.utils.collect_env.get_env_info()._asdict()
 
-        return {k: re.split(re_newline, str(v)) if "\n" in str(v) else v for k, v in info.items()}
+        env = torch.utils.collect_env.get_env_info()._asdict()
+        info = {
+            k: re.split(re_newline, str(v)) if "\n" in str(v) else v
+            for k, v in env.items()
+        }
+        cpu_info = info["cpu_info"]
+        cpu_infos = [info.split("=") for info in cpu_info]
+        cpu_info = {k: _val(v) for (k, v) in cpu_infos}
+        info["cpu_info"] = cpu_info
+
+        return info
     except Exception as e:
         return str(e)
 
@@ -137,6 +166,7 @@ def get_torch_sysinfo():
 def get_extensions(*, enabled):
 
     try:
+
         def to_json(x: extensions.Extension):
             return {
                 "name": x.name,
@@ -146,7 +176,11 @@ def get_extensions(*, enabled):
                 "remote": x.remote,
             }
 
-        return [to_json(x) for x in extensions.extensions if not x.is_builtin and x.enabled == enabled]
+        return [
+            to_json(x)
+            for x in extensions.extensions
+            if not x.is_builtin and x.enabled == enabled
+        ]
     except Exception as e:
         return str(e)
 
