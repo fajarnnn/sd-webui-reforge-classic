1a2cb58b824d2e2579e1a31ff63638ee830ee9c5
Haoming <hmstudy02@gmail.com>
2025-01-08 16:46:55 +0800

    models

--- modules/paths_internal.py
+++ modules/paths_internal.py
@@ -1,4 +1,6 @@
-"""this module defines internal paths used by program and is safe to import before dependencies are installed in launch.py"""
+"""
+this module defines internal paths used by program and is safe to import before dependencies are installed in launch.py
+"""
 
 import argparse
 import os
@@ -9,7 +11,7 @@ from pathlib import Path
 
 normalized_filepath = lambda filepath: str(Path(filepath).absolute())
 
-commandline_args = os.environ.get('COMMANDLINE_ARGS', "")
+commandline_args = os.environ.get("COMMANDLINE_ARGS", "")
 sys.argv += shlex.split(commandline_args)
 
 cwd = os.getcwd()
@@ -18,20 +20,20 @@ script_path = os.path.dirname(modules_path)
 
 sd_configs_path = os.path.join(script_path, "configs")
 sd_default_config = os.path.join(sd_configs_path, "v1-inference.yaml")
-sd_model_file = os.path.join(script_path, 'model.ckpt')
+sd_model_file = os.path.join(script_path, "model.ckpt")
 default_sd_model_file = sd_model_file
 
-# Parse the --data-dir flag first so we can use it as a base for our other argument default values
 parser_pre = argparse.ArgumentParser(add_help=False)
-parser_pre.add_argument("--data-dir", type=str, default=os.path.dirname(modules_path), help="base path where all user data is stored", )
-cmd_opts_pre = parser_pre.parse_known_args()[0]
+parser_pre.add_argument("--data-dir", type=str, default=os.path.dirname(modules_path), help="base path where all user data is stored")
+parser_pre.add_argument("--model-ref", type=str, default=None, help="base path for all models")
+cmd_opts_pre, _ = parser_pre.parse_known_args()
 
 data_path = cmd_opts_pre.data_dir
 
-models_path = os.path.join(data_path, "models")
+models_path = cmd_opts_pre.model_ref or os.path.join(data_path, "models")
 extensions_dir = os.path.join(data_path, "extensions")
 extensions_builtin_dir = os.path.join(script_path, "extensions-builtin")
 config_states_dir = os.path.join(script_path, "config_states")
 default_output_dir = os.path.join(data_path, "output")
 
-roboto_ttf_file = os.path.join(modules_path, 'Roboto-Regular.ttf')
+roboto_ttf_file = os.path.join(modules_path, "Roboto-Regular.ttf")
