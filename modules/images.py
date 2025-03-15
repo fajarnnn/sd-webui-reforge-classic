1144e98ec912dad682f4549a9169adb61e5c8497
Haoming <hmstudy02@gmail.com>
2025-03-15 18:55:03 +0800

    fix

--- modules/images.py
+++ modules/images.py
@@ -162,9 +162,13 @@ def draw_grid_annotations(im, width, height, hor_texts, ver_texts, margin=0):
         for line in lines:
             fnt = initial_fnt
             fontsize = initial_fontsize
-            while drawing.multiline_textsize(line.text, font=fnt)[0] > line.allowed_width and fontsize > 0:
-                fontsize -= 1
-                fnt = get_font(fontsize)
+            while True:
+                bbox = drawing.multiline_textbbox((0, 0), line.text, font=fnt)
+                if (bbox[2] - bbox[0]) > line.allowed_width and fontsize > 0:
+                    fontsize -= 1
+                    fnt = get_font(fontsize)
+                else:
+                    break
             drawing.multiline_text((draw_x, draw_y + line.size[1] / 2), line.text, font=fnt, fill=color_active if line.is_active else color_inactive, anchor="mm", align="center")
 
             if not line.is_active:
