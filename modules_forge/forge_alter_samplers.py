4c70eeb14cafa5bf0210a55b79c824fd5467b479
Haoming <hmstudy02@gmail.com>
2025-05-16 11:26:44 +0800

    simplify

--- modules_forge/forge_alter_samplers.py
+++ modules_forge/forge_alter_samplers.py
@@ -8,34 +8,23 @@ class AlterSampler(sd_samplers_kdiffusion.KDiffusionSampler):
         self.sampler_name = sampler_name
         self.scheduler_name = scheduler_name
         self.unet = sd_model.forge_objects.unet
-
-        match self.sampler_name:
-            case "euler_cfg_pp":
-                sampler_function = sd_samplers_cfgpp.sample_euler_cfg_pp
-            case "euler_ancestral_cfg_pp":
-                sampler_function = sd_samplers_cfgpp.sample_euler_ancestral_cfg_pp
-            case "dpmpp_sde_cfg_pp":
-                sampler_function = sd_samplers_cfgpp.sample_dpmpp_sde_cfg_pp
-            case "dpmpp_2m_cfg_pp":
-                sampler_function = sd_samplers_cfgpp.sample_dpmpp_2m_cfg_pp
-            case "dpmpp_3m_sde_cfg_pp":
-                sampler_function = sd_samplers_cfgpp.sample_dpmpp_3m_sde_cfg_pp
-            case _:
-                raise ValueError(f"Unknown sampler: {sampler_name}")
+        sampler_function = getattr(sd_samplers_cfgpp, f"sample_{self.sampler_name}", None)
+        if sampler_function is None:
+            raise ValueError(f"Unknown sampler: {sampler_name}")
 
         super().__init__(sampler_function, sd_model, None)
 
-    def sample(self, p, x, conditioning, unconditional_conditioning, steps=None, image_conditioning=None):
+    def sample(self, p, *args, **kwargs):
+        # self.scheduler_name = p.scheduler
         if p.cfg_scale > 2.0:
             logging.warning("Low CFG is recommended when using CFG++ samplers")
-        self.scheduler_name = p.scheduler
-        return super().sample(p, x, conditioning, unconditional_conditioning, steps, image_conditioning)
+        return super().sample(p, *args, **kwargs)
 
-    def sample_img2img(self, p, x, noise, conditioning, unconditional_conditioning, steps=None, image_conditioning=None):
+    def sample_img2img(self, p, *args, **kwargs):
+        # self.scheduler_name = p.scheduler
         if p.cfg_scale > 2.0:
             logging.warning("Low CFG is recommended when using CFG++ samplers")
-        self.scheduler_name = p.scheduler
-        return super().sample_img2img(p, x, noise, conditioning, unconditional_conditioning, steps, image_conditioning)
+        return super().sample_img2img(p, *args, **kwargs)
 
 
 def build_constructor(sampler_name):
