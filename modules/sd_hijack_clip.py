fbd0c5a3b5d8214a310e65991f5a696062afb0ff
Haoming <hmstudy02@gmail.com>
2025-06-23 10:09:48 +0800

    docstring

--- modules/sd_hijack_clip.py
+++ modules/sd_hijack_clip.py
@@ -3,7 +3,7 @@ from collections import namedtuple
 
 import torch
 
-from modules import prompt_parser, devices, sd_hijack, sd_emphasis
+from modules import devices, prompt_parser, sd_emphasis, sd_hijack
 from modules.shared import opts
 
 
@@ -11,8 +11,7 @@ class PromptChunk:
     """
     This object contains token ids, weight (multipliers:1.4) and textual inversion embedding info for a chunk of prompt.
     If a prompt is short, it is represented by one PromptChunk, otherwise, multiple are necessary.
-    Each PromptChunk contains an exact amount of tokens - 77, which includes one for start and end token,
-    so just 75 tokens from prompt.
+    Each PromptChunk contains an exact amount of tokens - 77, which includes one for start and end token each, and 75 tokens from prompt.
     """
 
     def __init__(self):
@@ -23,7 +22,7 @@ class PromptChunk:
 
 PromptChunkFix = namedtuple("PromptChunkFix", ["offset", "embedding"])
 """
-An object of this type is a marker showing that textual inversion embedding's vectors have to placed at offset in the prompt
+An object of this type is a marker showing that textual inversion embedding's vectors have to be placed at offset in the prompt
 chunk. Those objects are found in PromptChunk.fixes and, are placed into FrozenCLIPEmbedderWithCustomWordsBase.hijack.fixes, and finally
 are applied by sd_hijack.EmbeddingsWithFixes's forward function.
 """
@@ -31,17 +30,15 @@ are applied by sd_hijack.EmbeddingsWithFixes's forward function.
 
 class FrozenCLIPEmbedderWithCustomWordsBase(torch.nn.Module):
     """
-    A pytorch module that is a wrapper for FrozenCLIPEmbedder module. it enhances FrozenCLIPEmbedder,
-    making it possible to have unlimited prompt length and assign weights to tokens in prompt.
+    A pytorch module that is a wrapper for FrozenCLIPEmbedder module. It enhances FrozenCLIPEmbedder,
+    making it possible to have unlimited prompt length and assign weights to tokens in prompts.
     """
 
     def __init__(self, wrapped, hijack):
         super().__init__()
 
         self.wrapped = wrapped
-        """
-        Original FrozenCLIPEmbedder or FrozenOpenCLIPEmbedder module
-        """
+        """Original FrozenCLIPEmbedder or FrozenOpenCLIPEmbedder module"""
 
         self.hijack: sd_hijack.StableDiffusionModelHijack = hijack
         self.chunk_length = 75
@@ -51,7 +48,7 @@ class FrozenCLIPEmbedderWithCustomWordsBase(torch.nn.Module):
         self.legacy_ucg_val = None
 
     def empty_chunk(self):
-        """creates an empty PromptChunk and returns it"""
+        """Creates an empty PromptChunk and returns it"""
 
         chunk = PromptChunk()
         chunk.tokens = [self.id_start] + [self.id_end] * (self.chunk_length + 1)
@@ -59,7 +56,7 @@ class FrozenCLIPEmbedderWithCustomWordsBase(torch.nn.Module):
         return chunk
 
     def get_target_prompt_token_count(self, token_count):
-        """returns the maximum number of tokens a prompt of a known length can have before it requires one more PromptChunk to be represented"""
+        """Returns the maximum number of tokens a prompt of a known length can have before it requires one more PromptChunk to be represented"""
 
         return math.ceil(max(token_count, 1) / self.chunk_length) * self.chunk_length
 
@@ -70,10 +67,10 @@ class FrozenCLIPEmbedderWithCustomWordsBase(torch.nn.Module):
 
     def encode_with_transformers(self, tokens):
         """
-        converts a batch of token ids (in python lists) into a single tensor with numeric representation of those tokens;
+        Converts a batch of token ids (in python lists) into a single tensor with numeric representation of those tokens;
         All python lists with tokens are assumed to have same length, usually 77.
-        if input is a list with B elements and each element has T tokens, expected output shape is (B, T, C), where C depends on
-        model - can be 768 and 1024.
+        If input is a list with B elements and each element has T tokens, expected output shape is (B, T, C), where C depends on
+        model - can be 768 and 1280.
         Among other things, this call will read self.hijack.fixes, apply it to its inputs, and clear it (setting it to None).
         """
 
@@ -83,15 +80,14 @@ class FrozenCLIPEmbedderWithCustomWordsBase(torch.nn.Module):
         """
         Converts text into a tensor with this text's tokens' embeddings.
         Note that those are embeddings before they are passed through transformers.
-        nvpt is used as a maximum length in tokens. If text produces less teokens than nvpt, only this many is returned.
+        nvpt is used as a maximum length in tokens. If text produces less tokens than nvpt, only this many is returned.
         """
 
         raise NotImplementedError
 
     def tokenize_line(self, line):
         """
-        this transforms a single prompt into a list of PromptChunk objects - as many as needed to
-        represent the prompt.
+        This transforms a single prompt into a list of PromptChunk objects - as many as needed to represent the prompt.
         Returns the list and the total number of tokens in the prompt.
         """
 
@@ -110,7 +106,7 @@ class FrozenCLIPEmbedderWithCustomWordsBase(torch.nn.Module):
         def next_chunk(is_last=False):
             """
             puts current chunk into the list of results and produces the next one - empty;
-            if is_last is true, tokens <end-of-text> tokens at the end won't add to token_count
+            if is_last is true, tokens <end-of-text> tokens at the end won't be added to token_count
             """
             nonlocal token_count
             nonlocal last_comma
@@ -187,8 +183,8 @@ class FrozenCLIPEmbedderWithCustomWordsBase(torch.nn.Module):
 
     def process_texts(self, texts):
         """
-        Accepts a list of texts and calls tokenize_line() on each, with cache. Returns the list of results and maximum
-        length, in tokens, of all texts.
+        Accepts a list of texts and calls tokenize_line() on each, with cache.
+        Returns the list of results and maximum length, in tokens, of all texts.
         """
 
         token_count = 0
@@ -212,7 +208,7 @@ class FrozenCLIPEmbedderWithCustomWordsBase(torch.nn.Module):
         """
         Accepts an array of texts; Passes texts through transformers network to create a tensor with numerical representation of those texts.
         Returns a tensor with shape of (B, T, C), where B is length of the array; T is length, in tokens, of texts (including padding) - T will
-        be a multiple of 77; and C is dimensionality of each token - for SD1 it's 768, for SD2 it's 1024, and for SDXL it's 1280.
+        be a multiple of 77; and C is dimensionality of each token - for SD1 it's 768; for SDXL it's 1280.
         An example shape returned by this function can be: (2, 77, 768).
         For SDXL, instead of returning one tensor above, it returns a tuple with two: the other one with shape (B, 1280) with pooled values.
         Webui usually sends just one text at a time through this function - the only time when texts is an array with more than one element
@@ -261,15 +257,15 @@ class FrozenCLIPEmbedderWithCustomWordsBase(torch.nn.Module):
 
     def process_tokens(self, remade_batch_tokens, batch_multipliers):
         """
-        sends one single prompt chunk to be encoded by transformers neural network.
+        Sends one single prompt chunk to be encoded by transformers neural network.
         remade_batch_tokens is a batch of tokens - a list, where every element is a list of tokens; usually
         there are exactly 77 tokens in the list. batch_multipliers is the same but for multipliers instead of tokens.
-        Multipliers are used to give more or less weight to the outputs of transformers network. Each multiplier
-        corresponds to one token.
+        Multipliers are used to give more or less weight to the outputs of transformers network.
+        Each multiplier corresponds to one token.
         """
         tokens = torch.asarray(remade_batch_tokens).to(devices.device)
 
-        # this is for SD2: SD1 uses the same token for padding and end of text, while SD2 uses different ones.
+        # SD1 uses the same token for padding and end of text; while SDXL uses different ones
         if self.id_end != self.id_pad:
             for batch_pos in range(len(remade_batch_tokens)):
                 index = remade_batch_tokens[batch_pos].index(self.id_end)
